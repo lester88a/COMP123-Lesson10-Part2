c0f147bd111678324f3b685b4f8b690045994dc6
lester88a <lester88a@gmail.com>
2015-03-18 09:55:59 -0400

    Switch Images Button

--- COMP123-Lesson10-Part2/NewForm.Designer.cs
+++ COMP123-Lesson10-Part2/NewForm.Designer.cs
@@ -28,21 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPictureBox = new System.Windows.Forms.PictureBox();
+            this.NextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            this.MainPictureBox.Image = global::COMP123_Lesson10_Part2.Properties.Resources.Welcome_large;
+            this.MainPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.MainPictureBox.Name = "MainPictureBox";
+            this.MainPictureBox.Size = new System.Drawing.Size(280, 163);
+            this.MainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MainPictureBox.TabIndex = 0;
+            this.MainPictureBox.TabStop = false;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextButton.Location = new System.Drawing.Point(101, 181);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(93, 32);
+            this.NextButton.TabIndex = 1;
+            this.NextButton.Text = "NEXT";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
             // 
             // NewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 442);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.MainPictureBox);
             this.Name = "NewForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "New Form";
+            this.Load += new System.EventHandler(this.NewForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MainPictureBox;
+        private System.Windows.Forms.Button NextButton;
     }
 }
 
--- COMP123-Lesson10-Part2/NewForm.cs
+++ COMP123-Lesson10-Part2/NewForm.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Resources;
 
 namespace COMP123_Lesson10_Part2
 {
+    
     public partial class NewForm : Form
     {
+        private Bitmap[] _imageArray = { Properties.Resources.Welcome_large, 
+                                           Properties.Resources.welcome_balloons_banner, 
+                                           Properties.Resources.welcome_dis};
+        private int _imageIndex = 0;
+        //constructor+++++++++++++++++++++++++++++++++++++++++++++++++++++
         public NewForm()
         {
             InitializeComponent();
+
+            
+        }
+
+        //event handler form - form load event
+        private void NewForm_Load(object sender, EventArgs e)
+        {
+            //NextButton.Text = Properties.Resources.NextButtonStringS;
+            //this._immageArry[0] = Properties.Resources.welcome_balloons_banner;
+            MainPictureBox.Image = this._imageArray[this._imageIndex];
+
+        }
+
+        //event handler next button - click next
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if (this._imageIndex<this._imageArray.Length-1)
+            {
+                this._imageIndex++;
+            }
+            else
+            {
+                this._imageIndex = 0;
+            }
+            MainPictureBox.Image = this._imageArray[this._imageIndex];
         }
     }
 }
